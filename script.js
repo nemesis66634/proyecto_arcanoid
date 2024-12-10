f3a85c8a56396b6a57ac06e225abe1eb3a3cffa9
jacinto tenezaca <38246657-jacintotenezaca@users.noreply.replit.com>
2024-12-10 23:44:29 +0000

    version1

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+function empezar(valor) {
+    let ID = document.getElementById(`input${valor}`).value;
+    let URL = `https://pokeapi.co/api/v2/pokemon/${ID}`;
 
+    fetch(URL)
+    .then(response => response.json())
+    .then(data => mostrar(data))  
 
-
-
-
-
-
+    function mostrar(data) {
+        let mensaje = document.getElementById(`div${valor}`);
+        mensaje.innerHTML = `
+            <h1>${data.name}</h1>
+            <img src="${data.sprites.other['official-artwork'].front_default}">
+            <h2>${data.id}</h2>
+        `;
+    }   
+}
