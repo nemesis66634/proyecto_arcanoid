ed6a85caf80217dfd579415209882e45e0c898c7
nemesis66634 <jacintotenezaca875@gmail.com>
2024-12-11 22:02:39 +0000

    nuevo

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-function empezar(valor) {
-    let ID = document.getElementById(`input${valor}`).value;
-    let URL = `https://pokeapi.co/api/v2/pokemon/${ID}`;
 
-    fetch(URL)
-    .then(response => response.json())
-    .then(data => mostrar(data))  
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
 
-    function mostrar(data) {
-        let mensaje = document.getElementById(`div${valor}`);
-        mensaje.innerHTML = `
-            <h1>${data.name}</h1>
-            <img src="${data.sprites.other['official-artwork'].front_default}">
-            <h2>${data.id}</h2>
-        `;
-    }   
+canvas.width = 750
+canvas.height = 400
+
+const ballRadius = 4;
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
+
+
+function drawBall() {
+  ctx.beginPath(); 
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = 'red';
+  ctx.fill();
 }
+
+
+
+
+function draw(){
+  drawBall();
+  drawPaddle();
+  drawBricks();
+
+  collisionDetection();
+  ballmovement();
+  paddlemovement();
+  
+  windows.requestAnimationFrame(draw);
+  
+}
+
