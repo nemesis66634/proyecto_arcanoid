e4806ed50bdbd66943084bdd2ee64acba2453493
nemesis66634 <jacintotenezaca875@gmail.com>
2024-12-17 22:13:13 +0000

    nuevo

--- script.js
+++ script.js
@@ -2,47 +2,102 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 
+
 canvas.width = 750
 canvas.height = 400
 
-//variables
+
+//variables pelota
 const ballRadius = 4;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = -2;
 let dy = -2;
 
+
+//variables paleta
+const paddleHeight = 10;
+const paddleWidth = 50;
+let paddlex = (canvas.width - paddleWidth) / 2;
+let paddley  = canvas.height - 40;
+let rightPressed = false;
+let leftPressed = false;
+
+
+
+
 function cleanCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+
+
 
 function drawBall() {
   ctx.beginPath(); 
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = 'blue';
   ctx.fill();
+  ctx.closePath();
 }
 
 
+
+
+function drawPaddle(){
+  ctx.fillStyle = 'red';
+  ctx.fillRect(paddlex, paddley, paddleWidth,paddleHeight);
+}
+
+
+
+
 function ballMovement(){
-  if(
-      x + dx > canvas.width ||
-      x + dx < ballRadius
-  ){
+  if(x + dx > canvas.width || x + dx < ballRadius){
       dx = -dx;
   }
-
-  if(
-    y + dy > canvas.height ||
-    y + dy < ballRadius
-  ){
+  if(y + dy > canvas.height || y + dy < ballRadius){
     dy = -dy;
-  }
-
-
-  
+  }  
   x += dx;
   y += dy;
+}
+
+
+
+
+function initEvents(){
+  document.addEventListener('keydown', keyDownHandler);
+  document.addEventListener('keyup', keyUpHandler);
+  
+  function keyDownHandler(event){
+    const {key} = event;
+    if(key === 'Right' || key === 'ArrowRight'){
+      rightPressed = true;
+    }else if(key === 'left' || key === 'ArroLeft'){
+      leftPressed = true;
+    }
+  }
+  function keyUpHandler(event){
+    const {key} = event;
+    if(key === 'Right' || key === 'ArrowRight'){
+      rightPressed = false;
+    }else if(key === 'left' || key === 'ArroLeft'){
+      leftPressed = false;
+    }
+  }
+
+}
+
+
+
+
+function paddleMovement(){
+  if(rightPressed === 'true'){
+    paddlex += 7;  
+  }else if(rightPressed === 'true'){
+    paddlex -= 7;
+  }  
 }
 
 
@@ -51,15 +106,16 @@ function ballMovement(){
 function draw(){
   cleanCanvas();
   drawBall();
-  //drawPaddle();
+  drawPaddle();
   //drawBricks();
-
   //collisionDetection();
   ballMovement();
-  //paddleMovement();
-  
+  paddleMovement();
   window.requestAnimationFrame(draw);
-  
 }
 
+initEvents()
 draw();
+
+
+
